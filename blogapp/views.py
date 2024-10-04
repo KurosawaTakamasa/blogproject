1d3158f0fb3d25a90a6f4ede3ec3da56488a0602
Kurosawa Takamasa <ta.kurosawa.1990@gmail.com>
2024-10-04 14:56:42 +0900

    10/4 トップページ完成

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
-from django.views.generic.base import TemplateView
+from django.views.generic import ListView
+from .models import BlogPost
 
-class IndexView(TemplateView):
+class IndexView(ListView):
     #描画する(レンダリングする) HTMLファイルを指定
     template_name = 'index.html'
+    
+    #参照するモデルを指定
+    #model = BlogPost
+    
+    queryset = BlogPost.objects.order_by('-posted_at')
 
 
