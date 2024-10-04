e761afbae2cfdbcfc4520ceda3648cb12f88d849
Kurosawa Takamasa <ta.kurosawa.1990@gmail.com>
2024-10-04 13:15:58 +0900

    10/4 モデル作成

--- blogapp/admin.py
+++ blogapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import BlogPost
 
-# Register your models here.
+#管理サイトにBlogPostを登録する
+admin.site.register(BlogPost)
--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class BlogPost(models.Model):
+    #タイトル用フィールド
+    title = models.CharField(max_length=200,
+                             verbose_name='タイトル')
+    
+    #本文用のフィールド
+    content = models.TextField(verbose_name='本文')
+    
+    #投稿日時のフィールド
+    posted_at = models.DateTimeField(verbose_name='投稿日時',
+                                     auto_now_add=True)
+    
+    #カテゴリ用のフィールド
+    category = models.CharField(max_length=50,
+                                verbose_name='カテゴリ',
+                                choices=(('science','科学のこと' ),
+                                         ('dailylife', '日常生活のこと'),
+                                         ('music', '音楽のこと'))
+                                )
+    
+    def __str__(self):
+        return self.title
+    
